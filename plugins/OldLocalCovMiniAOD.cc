e29db2dad8f6eebc6aff17b110920aee594614d6
swmukher <s.mukherjee@cern.ch>
2021-03-07 20:00:50 +0100

    add pT>5 GeV cut

--- plugins/OldLocalCovMiniAOD.cc
+++ plugins/OldLocalCovMiniAOD.cc
@@ -18,10 +18,7 @@
 #include "CondFormats/DataRecord/interface/EcalPFRecHitThresholdsRcd.h"
 #include "Geometry/CaloTopology/interface/CaloTopology.h"
 #include "Geometry/CaloEventSetup/interface/CaloTopologyRecord.h"
-#include "Geometry/Records/interface/CaloTopologyRecord.h"
-#include "Geometry/CaloEventSetup/plugins/CaloTopologyBuilder.h"
 #include "FWCore/Framework/interface/EventSetup.h"
-#include "Geometry/CaloTopology/interface/CaloSubdetectorTopology.h"
 
 #include "TTree.h"
 
@@ -29,25 +26,20 @@ class OldLocalCovMiniAOD : public edm::one::EDAnalyzer<edm::one::SharedResources
 public:
   explicit OldLocalCovMiniAOD(const edm::ParameterSet&);
   ~OldLocalCovMiniAOD();
-  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
   edm::Service<TFileService> fs;
   TTree   *tree = fs->make<TTree>("EventTree", "EventData");
   std::vector<double>  eleSigmaIetaIetaOld;
   std::vector<double>  eleSigmaIetaIetaNew;
+  std::vector<float>  elePt;
 
 private:
-  void beginJob() override;
   void analyze(const edm::Event&, const edm::EventSetup&) override;
-  void endJob() override;
 
   edm::EDGetTokenT<edm::View<reco::GsfElectron> > eleToken_;
   edm::EDGetTokenT<EcalRecHitCollection> EBRecHitCollectionT_;
   edm::EDGetTokenT<EcalRecHitCollection> EERecHitCollectionT_;
   edm::ESHandle<CaloTopology> theCaloTopology;
 
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-  edm::ESGetToken<SetupData, SetupRecord> setupToken_;
-#endif
 };
 
 OldLocalCovMiniAOD::OldLocalCovMiniAOD(const edm::ParameterSet& iConfig)
@@ -62,6 +54,7 @@ OldLocalCovMiniAOD::OldLocalCovMiniAOD(const edm::ParameterSet& iConfig)
 
   tree->Branch("eleSigmaIetaIetaOld_",&eleSigmaIetaIetaOld);
   tree->Branch("eleSigmaIetaIetaNew_",&eleSigmaIetaIetaNew);
+  tree->Branch("elePt_",&elePt);
 
 }
 
@@ -77,8 +70,11 @@ void OldLocalCovMiniAOD::analyze(const edm::Event& iEvent, const edm::EventSetup
         
   eleSigmaIetaIetaOld.clear();
   eleSigmaIetaIetaNew.clear();
+  elePt.clear();
 
   for(const auto& ele : iEvent.get(eleToken_) ) {
+
+    if ( ele.pt() < 5.0 ) continue;
 
     const reco::SuperCluster& superClus = *ele.superCluster();
     const reco::CaloCluster &seedCluster = *superClus.seed();
@@ -87,40 +83,24 @@ void OldLocalCovMiniAOD::analyze(const edm::Event& iEvent, const edm::EventSetup
     edm::Handle<EcalRecHitCollection> EERecHits_;
     iEvent.getByToken(EBRecHitCollectionT_, EBRecHits_);
     iEvent.getByToken(EERecHitCollectionT_, EERecHits_);
-     const EcalRecHitCollection *recHits = iseb ? EBRecHits_.product() : EERecHits_.product();
-
-     iSetup.get<CaloTopologyRecord>().get(theCaloTopology);
-     const CaloTopology* caloTopology = theCaloTopology.product();
+    const EcalRecHitCollection *recHits = iseb ? EBRecHits_.product() : EERecHits_.product();
+    
+    iSetup.get<CaloTopologyRecord>().get(theCaloTopology);
+    const CaloTopology* caloTopology = theCaloTopology.product();
     
     using ClusterTools = noZS::EcalClusterTools;     
     std::vector<float> oldLocalCov = ClusterTools::localCovariances(seedCluster,recHits,caloTopology) ;
     float sigmaIetaIetaOld = sqrt(oldLocalCov[0]);
-
+    
     eleSigmaIetaIetaNew.push_back(ele.full5x5_sigmaIetaIeta());
     eleSigmaIetaIetaOld.push_back(sigmaIetaIetaOld);
-
+    elePt.push_back(ele.pt());
+    
   }
 
   tree->Fill();
 
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-  // if the SetupData is always needed
-  auto setup = iSetup.getData(setupToken_);
-  // if need the ESHandle to check if the SetupData was there or not
-  auto pSetup = iSetup.getHandle(setupToken_);
-#endif
 }
 
-void OldLocalCovMiniAOD::beginJob() {
-}
-
-void OldLocalCovMiniAOD::endJob() {
-}
-
-void OldLocalCovMiniAOD::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
-}
 
 DEFINE_FWK_MODULE(OldLocalCovMiniAOD);
